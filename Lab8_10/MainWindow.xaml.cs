3ea370f0566732709e944969b4d98740da41be53
����� <doctorlesh@mail.ru>
2016-12-22 18:20:37 +0300

    Add project files.

---
+++ Lab8_10/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab8_10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+                    }
+ 
+         
+ 
+         public string AssemblyTitle
+         {
+             get
+             {
+                 object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+                 if (attributes.Length > 0)
+                 {
+                     AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+                     if (titleAttribute.Title != "")
+                     {
+                         return titleAttribute.Title;
+                     }
+                 }
+                 return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+             }
+         }
+ 
+         public string AssemblyVersion
+         {
+             get
+             {
+                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+             }
+         }
+ 
+         public string AssemblyDescription
+         {
+             get
+             {
+                 object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+                 if (attributes.Length == 0)
+                 {
+                     return "";
+                 }
+                 return ((AssemblyDescriptionAttribute)attributes[0]).Description;
+             }
+         }
+ 
+         public string AssemblyProduct
+         {
+             get
+             {
+                 object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+                 if (attributes.Length == 0)
+                 {
+                     return "";
+                 }
+                 return ((AssemblyProductAttribute)attributes[0]).Product;
+             }
+         }
+ 
+         public string AssemblyCopyright
+         {
+             get
+             {
+                 object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+                 if (attributes.Length == 0)
+                 {
+                     return "";
+                 }
+                 return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+             }
+         }
+ 
+         public string AssemblyCompany
+         {
+             get
+             {
+                 object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+                 if (attributes.Length == 0)
+                 {
+                     return "";
+                 }
+                 return ((AssemblyCompanyAttribute)attributes[0]).Company;
+             }
+         }
+
+        public string Text { get; private set; }
+        
+    }
+ }
+    
+    
+
